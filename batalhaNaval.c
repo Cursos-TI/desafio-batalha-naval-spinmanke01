a493e0c1287a674ae57a5e3fd1a3b0365ffa4b2e
Miguel <miguelmsouto01@gmail.com>
2025-05-12 21:05:12 +0000

    batalhaNaval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
 
 int main() {
     char tabuleiro[10][10]; // Matriz para o tabuleiro
@@ -21,6 +18,14 @@ int main() {
     tabuleiro[5][1] = '3';
     tabuleiro[6][1] = '3';
     tabuleiro[7][1] = '3';
+    // navios diagonais
+    tabuleiro[7][5] = '3';
+    tabuleiro[6][6] = '3';
+    tabuleiro[5][7] = '3';
+
+    tabuleiro[3][1] = '3';
+    tabuleiro[4][2] = '3';
+    tabuleiro[5][3] = '3';
 
     printf("Tabuleiro Naval:\n");
     printf(" A B C D E F G H I J\n");
